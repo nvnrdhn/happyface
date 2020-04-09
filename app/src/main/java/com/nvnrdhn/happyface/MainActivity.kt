b80204773ca0fc75122de4e1086d12069814753b
Novan Ardh <NovanArdhn_@live.com>
2020-04-09 15:26:46 +0700

    camera preview implementation

--- app/src/main/java/com/nvnrdhn/happyface/MainActivity.kt
+++ app/src/main/java/com/nvnrdhn/happyface/MainActivity.kt
@@ -1,20 +1,83 @@
 package com.nvnrdhn.happyface
 
+import android.Manifest
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
 import androidx.preference.PreferenceManager
+import com.google.common.util.concurrent.ListenableFuture
+import kotlinx.android.synthetic.main.activity_main.*
+
+private const val REQUEST_CODE_PERMISSIONS = 10
+private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var cameraProviderFuture : ListenableFuture<ProcessCameraProvider>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
         PreferenceManager.getDefaultSharedPreferences(this).apply {
             if (!getBoolean(OnboardFragment.COMPLETED_ONBOARDING_PREF, false)) {
                 startActivity(Intent(this@MainActivity, OnboardingActivity::class.java))
                 finish()
             }
         }
+        if (allPermissionsGranted()) {
+            preview_view.post { startCamera() }
+        }
+        else {
+            requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                preview_view.post { startCamera() }
+            }
+            else {
+                Toast.makeText(this, "Permissions not granted.", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun startCamera() {
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider = cameraProviderFuture.get()
+            bindPreview(cameraProvider)
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    fun bindPreview(cameraProvider : ProcessCameraProvider) {
+        var preview : Preview = Preview.Builder()
+            .build()
+
+        var cameraSelector : CameraSelector = CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
+            .build()
+
+        var camera = cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
+        preview.setSurfaceProvider(preview_view.createSurfaceProvider(camera.cameraInfo))
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        checkSelfPermission(it) == PackageManager.PERMISSION_GRANTED
     }
 }
